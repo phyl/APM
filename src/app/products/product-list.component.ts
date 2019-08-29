3af5e6aaecb9c781fc6453bdee55229fca22444d
Phyllis <pmhindurwa@gmail.com>
2019-08-29 06:31:00 +0200

    Product List

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  products: any[] = [
+    {
+    "productId" : 2,
+    "productName" : "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2016",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "https://openclipart.org/image/300px/sv"
+  },
+  {
+  "productId" : 2,
+  "productName" : "Garden Cart",
+  "productCode": "GDN-0023",
+  "releaseDate": "March 18, 2016",
+  "description": "15 gallon capacity rolling garden cart",
+  "price": 32.99,
+  "starRating": 4.2,
+  "imageUrl": "https://openclipart.org/image/300px/sv"
+},
+  ]
 }
 
 
